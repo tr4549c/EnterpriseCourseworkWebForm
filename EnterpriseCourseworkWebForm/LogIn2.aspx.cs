a69f91c2d536873661cf600f2d9b4975e065911f
Liam Bennett <lb6282h@greenwich.ac.uk>
2020-04-10 16:05:30 +0100

    Put encryption back

--- EnterpriseCourseworkWebForm/LogIn2.aspx.cs
+++ EnterpriseCourseworkWebForm/LogIn2.aspx.cs
@@ -18,9 +18,8 @@ namespace EnterpriseCourseworkWebForm
         protected void loginB_Click(object sender, EventArgs e)
         {
    
-            //example login username: dignissim  password: DVD49JKR0J
-            // staffID = Database.LoginRegisteredStaff(TextBox1.Text, Encryption.Encrypt(TextBox2.Text));
-            staffID = Database.LoginRegisteredStaff(TextBox1.Text, (TextBox2.Text));
+            //example login username: dignissim  password: Password   (Admin --> admin, Manager -> manager)
+            staffID = Database.LoginRegisteredStaff(TextBox1.Text, Encryption.Encrypt(TextBox2.Text));
 
             if (staffID > 0)
             {
