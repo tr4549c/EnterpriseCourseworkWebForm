f064f4ed32ba4ec8cde48cc64750bbbeae314695
Luiza Plaiu <lp7619j@gre.ac.uk>
2020-04-10 15:14:04 +0100

    changes

--- EnterpriseCourseworkWebForm/Post SignInIdeaPage.aspx.designer.cs
+++ EnterpriseCourseworkWebForm/Post SignInIdeaPage.aspx.designer.cs
@@ -571,6 +571,15 @@ namespace EnterpriseCourseworkWebForm {
         protected global::System.Web.UI.WebControls.Label Label13;
         
         /// <summary>
+        /// one control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton one;
+        
+        /// <summary>
         /// two control.
         /// </summary>
         /// <remarks>
